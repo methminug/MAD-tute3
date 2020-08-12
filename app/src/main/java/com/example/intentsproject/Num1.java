305f901842af02e1fcfde6db17de2f9d84fc230d
Methmi Nugawela <methmi.nuga@gmail.com>
2020-08-12 17:03:11 +0530

    2nd Commit

--- app/src/main/java/com/example/intentsproject/Num1.java
+++ app/src/main/java/com/example/intentsproject/Num1.java
@@ -8,14 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Num1#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Num1 extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -27,14 +21,6 @@ public class Num1 extends Fragment {
     public Num1() {
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @return A new instance of fragment Num1.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Num1 newInstance(String param1) {
         Num1 fragment = new Num1();
         Bundle args = new Bundle();
