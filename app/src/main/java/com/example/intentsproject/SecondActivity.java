51e240ed35fbcf9485fa805b772b8616bfb11084
Methmi Nugawela <methmi.nuga@gmail.com>
2020-08-27 07:30:23 +0530

    3rd Commit

--- app/src/main/java/com/example/intentsproject/SecondActivity.java
+++ app/src/main/java/com/example/intentsproject/SecondActivity.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private String disp;
+    private float calculation;
+    private TextView calcText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,12 +20,50 @@ public class SecondActivity extends AppCompatActivity {
 
         //Retrieve intent and extras
         Intent intent = getIntent();
-        String num1 = intent.getStringExtra("firstNum");
-        String num2 = intent.getStringExtra("secondNum");
+        final String num1 = intent.getStringExtra("firstNum");
+        final String num2 = intent.getStringExtra("secondNum");
+        disp = "Choose an operation for "+num1+" and "+num2;
+        calcText = findViewById(R.id.textView);
+        calcText.setText(disp);
 
-        TextView txtView = findViewById(R.id.textView);
-        txtView.setText(num1);
-        txtView = findViewById(R.id.textView2);
-        txtView.setText(num2);
+        findViewById(R.id.plus).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculation = Integer.parseInt(num1) + Integer.parseInt(num2);
+                disp = num1 + " + " + num2 + " = " + calculation;
+                calcText = findViewById(R.id.textView);
+                calcText.setText(disp);
+            }
+        });
+
+        findViewById(R.id.minus).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculation = Integer.parseInt(num1) - Integer.parseInt(num2);
+                disp = num1 + " - " + num2 + " = " + calculation;
+                calcText = findViewById(R.id.textView);
+                calcText.setText(disp);
+            }
+        });
+
+        findViewById(R.id.divide).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculation = Integer.parseInt(num1) / (float)Integer.parseInt(num2);
+                disp = num1 + " / " + num2 + " = " + calculation;
+                calcText = findViewById(R.id.textView);
+                calcText.setText(disp);
+            }
+        });
+
+        findViewById(R.id.multip).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculation = Integer.parseInt(num1) * Integer.parseInt(num2);
+                disp = num1 + " x " + num2 + " = " + calculation;
+                calcText = findViewById(R.id.textView);
+                calcText.setText(disp);
+            }
+        });
     }
 }
